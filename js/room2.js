772f6a101f4864448236654cfc761d1607b809bd
charansadhu2009 <raji.vukkadapu@gmail.com>
2026-07-31 19:57:22 +0530

    Add interactive clues and unlock functionality

--- js/room2.js
+++ js/room2.js
@@ -1,1 +1,63 @@
+let clues=0;
 
+const message=document.getElementById("message");
+
+bookshelf.onclick=function(){
+
+message.innerHTML="Book Number 8";
+
+clues++;
+
+}
+
+painting.onclick=function(){
+
+message.innerHTML="Painting says BLUE = 4";
+
+clues++;
+
+}
+
+desk.onclick=function(){
+
+message.innerHTML="Desk contains old diary.";
+
+clues++;
+
+}
+
+ghost.onclick=function(){
+
+message.innerHTML="Ghost whispers '8412'";
+
+clues++;
+
+}
+
+unlock.onclick=function(){
+
+const password=answer.value;
+
+if(clues<4){
+
+alert("Find all clues first!");
+
+return;
+
+}
+
+if(password==="8412"){
+
+alert("🎉 Room 2 Completed!");
+
+window.location="leaderboard.html";
+
+}
+
+else{
+
+alert("Wrong Password!");
+
+}
+
+}
