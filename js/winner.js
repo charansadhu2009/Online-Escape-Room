e9ea09a2ae1dd27e1ec16466e644f11eadf53dc2
charansadhu2009 <raji.vukkadapu@gmail.com>
2026-07-31 21:02:45 +0530

    Add player and score display with playAgain function

--- js/winner.js
+++ js/winner.js
@@ -1,1 +1,30 @@
 
+const player =
+localStorage.getItem("playerName") || "Guest";
+
+const score =
+localStorage.getItem("score") || 0;
+
+document.getElementById("playerName").innerHTML =
+"👤 " + player;
+
+document.getElementById("score").innerHTML =
+score;
+
+function playAgain(){
+
+localStorage.clear();
+
+window.location.href="index.html";
+
+}
+
+function downloadCertificate(){
+
+alert(
+"🏆 Congratulations " +
+player +
+"! Your Escape Room Certificate is ready."
+);
+
+}
