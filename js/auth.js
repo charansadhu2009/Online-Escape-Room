4cd7b00c63dcba41b84c7bdaa35e2bcd584f5287
charansadhu2009 <raji.vukkadapu@gmail.com>
2026-07-31 16:52:55 +0530

    Implement user registration and login functionality
    Added registration and login features with localStorage integration for user data.

---
+++ js/auth.js
@@ -0,0 +1,61 @@
+// Register
+
+const registerForm=document.getElementById("registerForm");
+
+if(registerForm){
+
+registerForm.addEventListener("submit",function(e){
+
+e.preventDefault();
+
+const name=document.getElementById("name").value;
+
+const email=document.getElementById("regEmail").value;
+
+const password=document.getElementById("regPassword").value;
+
+localStorage.setItem("name",name);
+localStorage.setItem("email",email);
+localStorage.setItem("password",password);
+
+alert("Registration Successful!");
+
+window.location="login.html";
+
+});
+
+}
+
+// Login
+
+const loginForm=document.getElementById("loginForm");
+
+if(loginForm){
+
+loginForm.addEventListener("submit",function(e){
+
+e.preventDefault();
+
+const email=document.getElementById("email").value;
+
+const password=document.getElementById("password").value;
+
+const savedEmail=localStorage.getItem("email");
+
+const savedPassword=localStorage.getItem("password");
+
+if(email===savedEmail && password===savedPassword){
+
+alert("Login Successful!");
+
+window.location="story.html";
+
+}else{
+
+alert("Invalid Email or Password!");
+
+}
+
+});
+
+}
