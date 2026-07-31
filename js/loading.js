7df11b83a1a34981cd8e317bd601243ef87db590
charansadhu2009 <raji.vukkadapu@gmail.com>
2026-07-31 20:04:19 +0530

    Implement loading bar functionality

--- js/loading.js
+++ js/loading.js
@@ -1,1 +1,24 @@
 
+let width=0;
+
+const bar=document.getElementById("bar");
+
+const percent=document.getElementById("percent");
+
+const load=setInterval(function(){
+
+width++;
+
+bar.style.width=width+"%";
+
+percent.innerHTML=width+"%";
+
+if(width>=100){
+
+clearInterval(load);
+
+window.location="room2.html";
+
+}
+
+},40);
