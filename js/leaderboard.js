81dddab6367daa1fe7e6db1c3162364821caef3e
charansadhu2009 <raji.vukkadapu@gmail.com>
2026-07-31 20:00:35 +0530

    Implement leaderboard display and reset functionality

--- js/leaderboard.js
+++ js/leaderboard.js
@@ -1,1 +1,50 @@
 
+let leaderboard=[
+
+{
+
+name:"Player",
+
+score:localStorage.getItem("score")||1000
+
+}
+
+];
+
+function loadLeaderboard(){
+
+const body=document.getElementById("leaderboardBody");
+
+body.innerHTML="";
+
+leaderboard.sort((a,b)=>b.score-a.score);
+
+leaderboard.forEach((player,index)=>{
+
+body.innerHTML+=`
+
+<tr>
+
+<td>${index+1}</td>
+
+<td>${player.name}</td>
+
+<td>${player.score}</td>
+
+</tr>
+
+`;
+
+});
+
+}
+
+function playAgain(){
+
+localStorage.clear();
+
+window.location="index.html";
+
+}
+
+loadLeaderboard();
